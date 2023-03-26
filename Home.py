27276d3a8ad01e14d674ef5090eb33ce20e8cbd9
Jude Sajith <judesajith.aj@gmail.com>
2023-03-26 07:58:28 -0400

    initial commit

---
+++ Home.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import os 
+import openai
+
+
+
+inp = st.text_input("Enter you query")
+if st.button("Search"):
+    input = str(source) + "  :"+ str(input)
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=input,
+        temperature=0.56,
+        max_tokens=2066,
+        top_p=1,
+        frequency_penalty=0.35,
+        presence_penalty=0
+    )
+    out=  response.choices[0].text
+    st.write(out)
